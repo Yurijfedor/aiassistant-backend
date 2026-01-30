568d1695f6d29a6dc080eadecac3eabb3477e5d1
Yurijfedor <yurijfedor178@gmail.com>
2026-01-30 22:14:12 +0200

    update server and ai router

--- src/routes/ai.ts
+++ src/routes/ai.ts
@@ -1,42 +1,28 @@
 import { Router } from "express";
-import cors from "cors";
 import { openai } from "../openai/client";
 
 export const aiRouter = Router();
 
-// 🔥 ЯВНО обробляємо preflight для цього endpoint
-aiRouter.options(
-  "/stream",
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://aiassistant-test-frontend.vercel.app",
-    ],
-    methods: ["POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-  }),
-);
-
 aiRouter.post("/stream", async (req, res) => {
-  const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
-
-  const { input } = body;
+  const { input } = req.body;
 
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-  res.setHeader("X-Accel-Buffering", "no");
 
   const stream = await openai.responses.stream({
     model: "gpt-4.1-mini",
     input,
   });
+  console.log(stream);
 
   try {
     for await (const event of stream) {
+      console.log(JSON.stringify(event));
+
       res.write(`data: ${JSON.stringify(event)}\n\n`);
     }
-  } catch (err) {
+  } catch {
     res.write(`event: error\ndata: {}\n\n`);
   } finally {
     res.end();
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,25 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import cors from "cors";
-
 import { aiRouter } from "./routes/ai";
 
 const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://aiassistant-test-frontend.vercel.app",
-    ],
+    origin: "http://localhost:5173",
   }),
 );
 
-// app.use(express.json());
-app.use(express.text());
+// 🔥 ПОВЕРТАЄМО json
+app.use(express.json());
+
 app.use("/ai", aiRouter);
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`🚀 AI backend running on port ${port}`);
+  console.log(`🚀 AI backend running on http://localhost:${port}`);
 });
